80c2c16007eedcfaddf02163bac68326e0d6867b
Administrator <Administrator@WIN-NFSR3IEMN4B>
2017-09-27 14:45:17 -0600

    add code for SurveyServlet
    reading for age, income, gender into variables

--- src/java/sait/survey/SurveyServlet.java
+++ src/java/sait/survey/SurveyServlet.java
@@ -5,6 +5,11 @@
  */
 package sait.survey;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -18,17 +23,25 @@ import javax.servlet.http.HttpServletResponse;
  * @author Administrator
  */
 public class SurveyServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         getServletContext().getRequestDispatcher("/WEB-INF/survey.jsp").forward(request, response);
-  
+
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
+        String gender = request.getParameter("gender");
+        int income = Integer.parseInt(request.getParameter("income"));
+        int age = Integer.parseInt(request.getParameter("age"));
+
+       
+
         getServletContext().getRequestDispatcher("/WEB-INF/surveyComplete.jsp").forward(request, response);
     }
 }
