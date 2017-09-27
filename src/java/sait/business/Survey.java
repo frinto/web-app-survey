88e091a79dba8a4cf176f067a4fb571647d6196e
Administrator <Administrator@WIN-NFSR3IEMN4B>
2017-09-27 14:24:50 -0600

    added code for Survey Class and User Class turned them into java bean to used with expression language later

--- src/java/sait/business/Survey.java
+++ src/java/sait/business/Survey.java
@@ -5,10 +5,47 @@
  */
 package sait.business;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Administrator
  */
-public class Survey {
+public class Survey implements Serializable {
     
+    private String gender;
+    private int income;
+    private int age;
+    
+    public Survey()
+    {
+        
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+
 }
